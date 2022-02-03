56f8a2b51c0913745c5e51120e35d3c6b08d11f2
krystek77 <krystian.wrona77@gmail.com>
2022-02-03 14:16:15 +0100

    Create header

---
+++ src/constants/images.js
@@ -0,0 +1,9 @@
+import logo from '../assets/images/logo.png';
+import spoon from '../assets/images/spoon.png';
+import hero_01 from '../assets/images/hero_01.png';
+import hero_02 from '../assets/images/hero_02.png';
+import hero_03 from '../assets/images/hero_03.png';
+
+const images = { logo, spoon, hero_01, hero_02, hero_03 };
+
+export default images;
--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,7 +1,58 @@
 import React from 'react';
+// import { MdRestaurantMenu } from 'react-icons/md';
+// import { GiHamburgerMenu } from 'react-icons/gi';
 import './Header.css';
+import images from '../../constants/images';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="logo">
+        <img className="logo__image" src={images.logo} alt="logo" />
+      </div>
+      <ul className="navigation">
+        <li className="navigation__item">
+          <a href="#home" className="link link_letter_04">
+            Home
+          </a>
+        </li>
+        <li className="navigation__item">
+          <a href="#pages" className="link link_letter_04">
+            Pages
+          </a>
+        </li>
+        <li className="navigation__item">
+          <a href="#contact" className="link link_letter_04">
+            Contact Us
+          </a>
+        </li>
+        <li className="navigation__item">
+          <a href="#blog" className="link link_letter_04">
+            Blog
+          </a>
+        </li>
+        <li className="navigation__item navigation__item_mr_0">
+          <a href="landing" className="link link_letter_04">
+            Landing
+          </a>
+        </li>
+      </ul>
+      <div className="header__login">
+        <a className="link link_letter_07" href="#login">
+          Log In / Registration
+        </a>
+        <div className="header__divider" />
+        <a className="link link_letter_07" href="#book-a-table">
+          Book Table
+        </a>
+      </div>
+      {/* <div>
+        <GiHamburgerMenu />
+        <div>
+          <MdRestaurantMenu />
+        </div>
+      </div> */}
+    </div>
+  );
 };
 export default Header;
