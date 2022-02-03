d1dea6627a742b65fb4fef90cf9f49e0f61acc23
krystek77 <krystian.wrona77@gmail.com>
2022-02-03 21:35:06 +0100

    Make a responsive navigation

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,41 +1,27 @@
-import React from 'react';
-// import { MdRestaurantMenu } from 'react-icons/md';
-// import { GiHamburgerMenu } from 'react-icons/gi';
-import './Header.css';
+import React, {useState} from 'react';
+import { MdRestaurantMenu } from 'react-icons/md';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
 import images from '../../constants/images';
+import './Header.css';
 
 const Header = () => {
+  const [turnMenu, setTurnMenu] = useState(false);
+
+  const openMenu = () => setTurnMenu(true);
+  const closeMenu = () => setTurnMenu(false);
+
   return (
     <div className="header">
       <div className="logo">
         <img className="logo__image" src={images.logo} alt="logo" />
       </div>
       <ul className="navigation">
-        <li className="navigation__item">
-          <a href="#home" className="link link_letter_04">
-            Home
-          </a>
-        </li>
-        <li className="navigation__item">
-          <a href="#pages" className="link link_letter_04">
-            Pages
-          </a>
-        </li>
-        <li className="navigation__item">
-          <a href="#contact" className="link link_letter_04">
-            Contact Us
-          </a>
-        </li>
-        <li className="navigation__item">
-          <a href="#blog" className="link link_letter_04">
-            Blog
-          </a>
-        </li>
-        <li className="navigation__item navigation__item_mr_0">
-          <a href="landing" className="link link_letter_04">
-            Landing
-          </a>
-        </li>
+        <li className="navigation__item"><a href="#home" className="link link_letter_04"> Home </a> </li>
+        <li className="navigation__item"><a href="#pages" className="link link_letter_04"> Pages </a> </li>
+        <li className="navigation__item"><a href="#contact" className="link link_letter_04"> Contact Us </a> </li>
+        <li className="navigation__item"><a href="#blog" className="link link_letter_04"> Blog </a> </li>
+        <li className="navigation__item navigation__item_mr_0"> <a href="landing" className="link link_letter_04"> Landing </a> </li>
       </ul>
       <div className="header__login">
         <a className="link link_letter_07" href="#login">
@@ -46,12 +32,22 @@ const Header = () => {
           Book Table
         </a>
       </div>
-      {/* <div>
-        <GiHamburgerMenu />
-        <div>
-          <MdRestaurantMenu />
-        </div>
-      </div> */}
+
+      <div className="navigation-small-screen">
+        <GiHamburgerMenu className='navigation-small-screen__open-button' onClick={openMenu}/>
+        {turnMenu && (
+          <div className='navigation-small-screen__overlay slide-bottom' onClick={closeMenu}>
+            <MdRestaurantMenu className='navigation-small-screen__close-button' />
+            <ul className='navigation-small-screen__menu'>
+              <li className="navigation-small-screen__item"><a href="#home" className="link navigation-small-screen__link link_letter_04"> Home </a> </li>
+              <li className="navigation-small-screen__item"><a href="#pages" className="link navigation-small-screen__link link_letter_04"> Pages </a> </li>
+              <li className="navigation-small-screen__item"><a href="#contact" className="link navigation-small-screen__link link_letter_04"> Contact Us </a> </li>
+              <li className="navigation-small-screen__item"><a href="#blog" className="link navigation-small-screen__link link_letter_04"> Blog </a> </li>
+              <li className="navigation-small-screen__item"><a href="landing" className="link navigation-small-screen__link link_letter_04"> Landing </a> </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
