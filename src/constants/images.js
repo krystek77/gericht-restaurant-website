04c7f480c77b531e9c0b83890eda8967caf02ddb
krystek77 <krystian.wrona77@gmail.com>
2022-02-05 12:55:08 +0100

    Import necessary images

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,13 @@
-import logo from '../assets/images/logo.png';
-import spoon from '../assets/images/spoon.png';
-import hero_01 from '../assets/images/hero_01.png';
-import hero_02 from '../assets/images/hero_02.png';
-import hero_03 from '../assets/images/hero_03.png';
+import logo from "../assets/images/logo.png";
+import spoon from "../assets/images/spoon.png";
+import hero_01 from "../assets/images/hero_01.png";
+import hero_02 from "../assets/images/hero_02.png";
+import hero_03 from "../assets/images/hero_03.png";
 
-const images = { logo, spoon, hero_01, hero_02, hero_03 };
+import G_SVG from "../assets/images/G.svg";
+import G_png from "../assets/images/G.png";
+import knife from "../assets/images/knife.png";
+
+const images = { logo, spoon, hero_01, hero_02, hero_03, G_SVG, G_png, knife };
 
 export default images;
--- src/containers/AboutUs/AboutUs.js
+++ src/containers/AboutUs/AboutUs.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import './AboutUs.css';
+import images from '../../constants/images';
 
 const AboutUs = () => {
   return <div>AboutUs</div>;
