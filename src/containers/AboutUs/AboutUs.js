254a7d3d0dd4e81ec08b8474758a2e22519188ae
krystek77 <krystian.wrona77@gmail.com>
2022-02-06 14:58:45 +0100

    Add media query for about us section

--- src/containers/AboutUs/AboutUs.js
+++ src/containers/AboutUs/AboutUs.js
@@ -11,20 +11,20 @@ const AboutUs = () => {
       </div>
 
       <div className='about-us__info'>
-        <h2 className='title mr_bottom-05'>About Us</h2>
-        <img className='spoon mr_bottom-2' src={images.spoon} alt='spoon' />
-        <p className='description description_align_right mr_bottom-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <h2 className='title title_wrap_no mr_bottom-05'>About Us</h2>
+        <img className='spoon mr_bottom-2 spoon_direction_left' src={images.spoon} alt='spoon' />
+        <p className='description description_align_left-reverse mr_bottom-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
         <button className='button'>Know More</button>
       </div>
 
       <div className='about-us__knife'>
         <img src={images.knife} alt='knife' />
-      </div>
+      </div>      
 
       <div className='about-us__history'>
-        <h2 className='title mr_bottom-05'>Our History</h2>
-        <img className='spoon mr_bottom-2 spoom_reverse ' src={images.spoon} alt='spoon' />
-        <p className='description mr_bottom-2'>Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
+        <h2 className='title title_wrap_no mr_bottom-05'>Our History</h2>
+        <img className='spoon mr_bottom-2 spoon_direction_right' src={images.spoon} alt='spoon' />
+        <p className='description description_align_left mr_bottom-2'>Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
         <button className='button'>Know More</button>
       </div>
     </div>
