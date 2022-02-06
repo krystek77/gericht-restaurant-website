96c72a655b3bc46828218300a98abcbe28969d94
krystek77 <krystian.wrona77@gmail.com>
2022-02-06 13:10:50 +0100

    Finish the desktop version of about us section

--- src/containers/AboutUs/AboutUs.js
+++ src/containers/AboutUs/AboutUs.js
@@ -1,9 +1,33 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import './AboutUs.css';
-import images from '../../constants/images';
+import React from "react";
+import "./AboutUs.css";
+import images from "../../constants/images";
 
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <div className='about-us'>
+      <div className='about-us__background'>
+        <img className='about-us__g-letter' src={images.G_SVG} alt='G letter' />
+      </div>
+
+      <div className='about-us__info'>
+        <h2 className='title mr_bottom-05'>About Us</h2>
+        <img className='spoon mr_bottom-2' src={images.spoon} alt='spoon' />
+        <p className='description description_align_right mr_bottom-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button className='button'>Know More</button>
+      </div>
+
+      <div className='about-us__knife'>
+        <img src={images.knife} alt='knife' />
+      </div>
+
+      <div className='about-us__history'>
+        <h2 className='title mr_bottom-05'>Our History</h2>
+        <img className='spoon mr_bottom-2 spoom_reverse ' src={images.spoon} alt='spoon' />
+        <p className='description mr_bottom-2'>Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
+        <button className='button'>Know More</button>
+      </div>
+    </div>
+  );
 };
 export default AboutUs;
