28c4b47871c573e07fc94f9a35dfc1750e518619
krystek77 <krystian.wrona77@gmail.com>
2022-02-10 10:45:11 +0100

    Finish menu section build

--- src/containers/Menu/Menu.js
+++ src/containers/Menu/Menu.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import Bite from '../../components/Bite/Bite';
 import './Menu.css';
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="menu background_glassful">
+      <div className="menu__overlay"></div>
+      <Bite position="top-left" />
+      <ul className="menu__items">
+        <li className="menu__item mr_bottom-4">
+          <a className="menu__link" href="#barmenu">
+            Bar Menu
+          </a>
+        </li>
+        <li className="menu__item mr_bottom-4">
+          <a className="menu__link" href="#foodmenu">
+            Food Menu
+          </a>
+        </li>
+        <li className="menu__item">
+          <a className="menu__link" href="#dessertmenu">
+            Dessert Menu
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 export default Menu;
