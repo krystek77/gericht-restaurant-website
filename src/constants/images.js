f67d90dbdf7f7bf9a85fa7bef3edf3f0cfe57039
krystek77 <krystian.wrona77@gmail.com>
2022-02-10 09:21:15 +0100

    Finish reservation section

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,13 +1,26 @@
-import logo from "../assets/images/logo.png";
-import spoon from "../assets/images/spoon.png";
-import hero_01 from "../assets/images/hero_01.png";
-import hero_02 from "../assets/images/hero_02.png";
-import hero_03 from "../assets/images/hero_03.png";
+import logo from '../assets/images/logo.png';
+import spoon from '../assets/images/spoon.png';
+import hero_01 from '../assets/images/hero_01.png';
+import hero_02 from '../assets/images/hero_02.png';
+import hero_03 from '../assets/images/hero_03.png';
 
-import G_SVG from "../assets/images/G.svg";
-import G_png from "../assets/images/G.png";
-import knife from "../assets/images/knife.png";
+import G_SVG from '../assets/images/G.svg';
+import G_png from '../assets/images/G.png';
+import knife from '../assets/images/knife.png';
+import arrow_down_SVG from '../assets/images/arrow_down.svg';
+import arrow_down_PNG from '../assets/images/arrow_down.png';
 
-const images = { logo, spoon, hero_01, hero_02, hero_03, G_SVG, G_png, knife };
+const images = {
+  logo,
+  spoon,
+  hero_01,
+  hero_02,
+  hero_03,
+  G_SVG,
+  G_png,
+  knife,
+  arrow_down_SVG,
+  arrow_down_PNG,
+};
 
 export default images;
--- src/containers/Reservation/Reservation.js
+++ src/containers/Reservation/Reservation.js
@@ -1,41 +1,109 @@
-import React from "react";
-import "./Reservation.css";
-import images from "../../constants/images";
+import React from 'react';
+import './Reservation.css';
+import images from '../../constants/images';
 
 const Reservation = () => {
   return (
-    <div className='reservation'>
-      <div className='subtitle mr_bottom-05 flex_center'>
-        <p className='subtitle__text mr_bottom-05'>Reservation</p>
-        <img className='subtitle__spoon' src={images.spoon} alt='spoon' />
+    <div className="reservation">
+      <div className="reservation__inner">
+        <div className="subtitle mr_bottom-05 flex_center">
+          <p className="subtitle__text mr_bottom-05">Reservation</p>
+          <img className="subtitle__spoon" src={images.spoon} alt="spoon" />
+        </div>
+        <h2 className="title title_wrap_no mr_bottom-3">Book A Table</h2>
+        <div className="reservation__form mr_bottom-3">
+          <div className="select">
+            <div className="select__icons">
+              <img
+                className="select__icon"
+                src={images.arrow_down_SVG}
+                alt="arrow down"
+              />
+            </div>
+            <select className="select__input" required>
+              <option className="select__option" value="1">
+                1 person
+              </option>
+              <option className="select__option" value="2">
+                2 persons
+              </option>
+              <option className="select__option" value="3">
+                3 persons
+              </option>
+              <option className="select__option" value="4">
+                4 persons
+              </option>
+              <option className="select__option" value="5">
+                5 persons
+              </option>
+              <option className="select__option" value="6">
+                6 persons
+              </option>
+            </select>
+          </div>
+          <div className="select">
+            <div className="select__icons">
+              <img
+                className="select__icon"
+                src={images.arrow_down_SVG}
+                alt="arrow down"
+              />
+            </div>
+            <select className="select__input" required>
+              <option className="select__option" value="1">
+                12/05/2022
+              </option>
+              <option className="select__option" value="2">
+                18/05/2022
+              </option>
+              <option className="select__option" value="3">
+                11/06/2022
+              </option>
+              <option className="select__option" value="4">
+                12/05/2022
+              </option>
+              <option className="select__option" value="5">
+                10/03/2022
+              </option>
+              <option className="select__option" value="6">
+                12/04/2022
+              </option>
+            </select>
+          </div>
+          <div className="select">
+            <div className="select__icons">
+              <img
+                className="select__icon"
+                src={images.arrow_down_SVG}
+                alt="arrow down"
+              />
+            </div>
+            <select className="select__input" required>
+              <option className="select__option" value="1">
+                11.00 p.m.
+              </option>
+              <option className="select__option" value="2">
+                2 p.m.
+              </option>
+              <option className="select__option" value="3">
+                2.00 a.m.
+              </option>
+              <option className="select__option" value="4">
+                9.00 p.m.
+              </option>
+              <option className="select__option" value="5">
+                6.00 p.m.
+              </option>
+              <option className="select__option" value="6">
+                3 a.m.
+              </option>
+            </select>
+          </div>
+        </div>
+        <button type="button" className="button">
+          Book Now
+        </button>
       </div>
-      <h2 className='title title_wrap_no mr_bottom-3'>Book A Table</h2>
-      <div className="reservation__inputs mr_bottom-3">
-        <div className="custom-select">
-          <select className="custom-select__select-input" name="persons" required >
-            <option className="custom-select__option" value="1">Person 1</option>
-            <option className="custom-select__option" value="2">Person 2</option>
-            <option className="custom-select__option" value="3">Person 3</option>
-            <option className="custom-select__option" value="4">Person 4</option>
-          </select>
-        </div>
-        <div className="custom-select">
-          <select className="custom-select__select-input" name="date" required >
-            <option className="custom-select__option" value="1">12/05/2021</option>
-            <option className="custom-select__option" value="2">17/05/2022</option>
-          </select>
-        </div>
-        <div className="custom-select">
-          <select className="custom-select__select-input" name="hour" required >
-            <option className="custom-select__option" value="1">11.00 AM</option>
-            <option className="custom-select__option" value="2">9.00 AM</option>
-            <option className="custom-select__option" value="3">15.00 AM</option>
-          </select>
-        </div>
-      </div>
-      <button type='button' className='button'>
-        Book Now
-      </button>
     </div>
   );
 };
