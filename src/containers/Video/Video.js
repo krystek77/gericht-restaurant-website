e08db17d24267c6acc774c99c44afaa4745ab325
krystek77 <krystian.wrona77@gmail.com>
2022-02-11 15:49:47 +0100

    Finish logic to handle video

--- src/containers/Video/Video.js
+++ src/containers/Video/Video.js
@@ -1,30 +1,30 @@
 /* eslint-disable no-unused-vars */
-import React, { useRef, useEffect } from 'react';
-import { VscPlay, VscPause } from 'react-icons/vsc';
-import videos from '../../constants/videos';
-import './Video.css';
+import React, { useRef, useState } from "react";
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import videos from "../../constants/videos";
+import "./Video.css";
 
 const Video = () => {
+  const [isPlayed, setIsPlayed] = useState(false);
   const ref = useRef(null);
 
   const handleVideo = () => {
-    if (ref.current.paused) {
+    if (isPlayed) {
+      ref.current.pause();
+      setIsPlayed(false);
+    } else {
       ref.current.play();
-    } else ref.current.pause();
+      setIsPlayed(true);
+    }
   };
   return (
-    <div className="video-section">
-      <video className="video-section__video" muted loop ref={ref}>
-        <source src={videos.video01} type="video/mp4" />
+    <div className='video-section'>
+      <video className='video-section__video' muted loop ref={ref}>
+        <source src={videos.video01} type='video/mp4' />
       </video>
-      <div className="video-section__overlay">
-        <button
-          className="video-section__play-button"
-          type="button"
-          onClick={handleVideo}
-        >
-          <VscPlay className="video-section__play-icon" />
-          {/* <VscPlay className="video-section__pause-icon" /> */}
+      <div className='video-section__overlay'>
+        <button className='video-section__play-button' type='button' onClick={handleVideo}>
+          {isPlayed ? <BsFillPauseFill className='video-section__pause-icon' /> : <BsFillPlayFill className='video-section__play-icon' />}
         </button>
       </div>
     </div>
