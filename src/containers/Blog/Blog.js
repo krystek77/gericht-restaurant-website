ee39dd1bccf53bca48b4b49f168190928772d301
krystek77 <krystian.wrona77@gmail.com>
2022-02-11 10:50:52 +0100

    Fake blog data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,34 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import images from '../../constants/images';
 import './Blog.css';
+
+const blogData = [
+  {
+    title: 'tips for prepping and caring for your grill',
+    image: images.blog01,
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.',
+    date: new Date(2021, 3, 16).toLocaleDateString(),
+    author: 'Annalisa L',
+  },
+  {
+    title: 'summer cocktails and mocktails',
+    image: images.blog01,
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.',
+    date: new Date(2021, 4, 23).toLocaleDateString(),
+    author: 'John Michael',
+  },
+  {
+    title: 'easy cooking for college students',
+    image: images.blog01,
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mus in.',
+    date: new Date(2021, 7, 6).toLocaleDateString(),
+    author: 'Fred W',
+  },
+];
 
 function Blog() {
   return <div className="Blog">some Blog</div>;
