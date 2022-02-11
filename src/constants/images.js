c66f04a09d06ebd7713676b168c721d88a7cc0db
krystek77 <krystian.wrona77@gmail.com>
2022-02-11 16:01:12 +0100

    Start chef section build

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,24 +1,26 @@
-import logo from '../assets/images/logo.png';
-import spoon from '../assets/images/spoon.png';
-import hero_01 from '../assets/images/hero_01.png';
-import hero_02 from '../assets/images/hero_02.png';
-import hero_03 from '../assets/images/hero_03.png';
+import logo from "../assets/images/logo.png";
+import spoon from "../assets/images/spoon.png";
+import hero_01 from "../assets/images/hero_01.png";
+import hero_02 from "../assets/images/hero_02.png";
+import hero_03 from "../assets/images/hero_03.png";
 
-import G_SVG from '../assets/images/G.svg';
-import G_png from '../assets/images/G.png';
-import knife from '../assets/images/knife.png';
-import arrow_down_SVG from '../assets/images/arrow_down.svg';
-import arrow_down_PNG from '../assets/images/arrow_down.png';
-import bite_delight from '../assets/images/bite_delight.png';
-import special from '../assets/images/special.png';
-import gallery01 from '../assets/images/gallery01.png';
-import gallery02 from '../assets/images/gallery02.png';
-import gallery03 from '../assets/images/gallery03.png';
-import gallery04 from '../assets/images/gallery04.png';
-import gallery05 from '../assets/images/gallery05.png';
-import blog01 from '../assets/images/blog01.png';
-import blog02 from '../assets/images/blog02.png';
-import blog03 from '../assets/images/blog03.png';
+import G_SVG from "../assets/images/G.svg";
+import G_png from "../assets/images/G.png";
+import knife from "../assets/images/knife.png";
+import arrow_down_SVG from "../assets/images/arrow_down.svg";
+import arrow_down_PNG from "../assets/images/arrow_down.png";
+import bite_delight from "../assets/images/bite_delight.png";
+import special from "../assets/images/special.png";
+import gallery01 from "../assets/images/gallery01.png";
+import gallery02 from "../assets/images/gallery02.png";
+import gallery03 from "../assets/images/gallery03.png";
+import gallery04 from "../assets/images/gallery04.png";
+import gallery05 from "../assets/images/gallery05.png";
+import blog01 from "../assets/images/blog01.png";
+import blog02 from "../assets/images/blog02.png";
+import blog03 from "../assets/images/blog03.png";
+import chef from "../assets/images/chef.png";
+import quote from '../assets/images/quote.png'
 
 const images = {
   logo,
@@ -41,6 +43,8 @@ const images = {
   blog01,
   blog02,
   blog03,
+  chef,
+  quote
 };
 
 export default images;
