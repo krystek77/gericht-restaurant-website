8fe17b3980cb1c1e79a17ba0328e4c3cdc932573
krystek77 <krystian.wrona77@gmail.com>
2022-02-12 15:12:45 +0100

    Add quote to customer item

--- src/components/CustomerItem/CustomerItem.js
+++ src/components/CustomerItem/CustomerItem.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Description from '../Description/Description'
 import "./CustomerItem.css";
+import images from '../../constants/images'
 
 function CustomerItem({ image, info, name, job }) {
   return (
     <div className='customer-card'>
       <div className='customer-card__image-wrapper'>
         <img className='customer-card__image' src={image} alt={name} />
+        <img className="customer-card__quote" src={images.quote} alt="quote" />
       </div>
       <div className='customer-card__content'>
         <Description classes="description_font-style_italic description_mr_bottom-1">{info}</Description>
